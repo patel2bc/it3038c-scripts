85451d234ef1b14775d9fa748e3fa34b7ed094c8
patel2bc <patel2bc@mail.uc.edu>
2023-01-24 19:14:56 -0500

    powershell files

---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,11 @@
+﻿function getIP{
+(get.netipaddress).ipv4address | Select.Script "192*"
+}
+
+$IP = getIP
+$USER = 
+$PS = $HOST.Version 
+$HN
+
+Write-Host = ("This machine's IP is $IP. User is $USER. Hostname is $HN. PowerShell Version is $PS. Today's Date is..."
+
