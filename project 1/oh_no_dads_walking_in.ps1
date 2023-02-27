6ae8519b3993d3f5febe815c3ad03ebb373f633e
patel2bc <122746019+patel2bc@users.noreply.github.com>
2023-02-26 22:47:17 -0500

    Update oh_no_dads_walking_in.ps1
    proj 1 resubmit 2/26/23 10:47

--- project 1/oh_no_dads_walking_in.ps1
+++ project 1/oh_no_dads_walking_in.ps1
@@ -1,9 +1,9 @@
-﻿# A student's worst nightmare is when your dad or mom walks in while your gaming when you're supposed to be doing your homework. 
-# With this powerful powershell script, you no longer have to worry! It will instantly minimize all apps and open UC's main websites 
+# A students worst nightmare is when your dad or mom walks in while your gaming when youre supposed to be doing your homework. 
+# With this powerful powershell script, you no longer have to worry. It will instantly minimize all apps and open UCs main websites 
 # along with a lecture and the some legit looking apps you may need for your homework. 
 
-# Instructions, simply double click on the ps script
-# Pro tip: Most gaming keyboards have a set of macro keys, I set mine to launch this script when pressed. 
+# Instructions, simply double click on the ps script (you may have to run it via PS ISE) 
+# Pro tip Most gaming keyboards have a set of macro keys, I set mine to launch this script when pressed. 
 
 # Minimize all apps
 $shell = New-Object -ComObject Shell.Application
@@ -20,10 +20,10 @@ Start-Process calc -WindowStyle Minimized
 Start-Process notepad -WindowStyle Minimized
 Start-Process excel -WindowStyle Minimized
 
-# This script should've open your default broswer, then open the following sites, launched notepad, excel, and notepad apps. 
+# This script shouldve open your default broswer, then open the following sites, launched notepad, excel, and notepad apps. 
+# If you get an error that says that you're not allowed to run scripts on this ccomptuer, type "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned" and hit Enter 
 
 # By Bunci Patel 
 # Sources: 
 # https://stackoverflow.com/questions/70692306/need-to-minimize-a-particular-window
 # https://www.youtube.com/watch?v=nePN6hPoLCg
-
